2b49e6ed128e84d813b2a1f6d516076bc0621afd
Ruhit Paul <ruhitpaul18@gmail.com>
2026-03-05 15:43:08 +0600

    word meaning section added

--- script/index.js
+++ script/index.js
@@ -1,3 +1,18 @@
+const createElements = (arr) => {
+    const htmlElements = arr.map((el) => `<div class="badge badge-soft badge-info text-lg">${el}</div>`);
+    return htmlElements.join(' ')
+}
+
+const manageSpinner = (status) => {
+    if(status) {
+        document.getElementById('spinner').classList.remove('hidden');
+        document.getElementById('word-container').classList.add('hidden');
+    } else {
+        document.getElementById('word-container').classList.remove('hidden');
+        document.getElementById('spinner').classList.add('hidden');
+    }
+}
+
 const loadLessons = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all')
         .then((res) => res.json())
@@ -17,6 +32,7 @@ const displayLesson = (lessons) => {
                 btn.classList.remove('btn-active');
             }
             button.classList.add('btn-active')
+            manageSpinner(true)
             const url = `https://openapi.programming-hero.com/api/level/${lesson.level_no}`
             fetch(url)
                 .then((res) => res.json())
@@ -24,6 +40,35 @@ const displayLesson = (lessons) => {
         })
         levelContainer.append(button);
     }
+}
+
+const loadWordDetail = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`
+    const res = await fetch(url);
+    const details = await res.json();
+    displayWordDetails(details.data)
+}
+
+const displayWordDetails = (word) => {
+    const detailsBox = document.getElementById('details-container');
+    detailsBox.innerHTML = `
+    <div>
+        <h2 class="font-semibold text-2xl">${word.word} (<i class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})</h2>
+    </div> 
+    <div>
+        <h2 class="font-semibold text-lg">Meaning</h2>
+        <p class="font-medium text-lg">${word.meaning}</p>
+    </div>
+    <div>
+        <h2 class="font-semibold text-lg">Example</h2>
+        <p class="font-medium text-lg">${word.sentence}</p>
+    </div>
+    <div>
+        <h2 class="font-semibold text-lg">Synonyms</h2>
+        <div class="flex gap-3 mt-2">${createElements(word.synonyms)}</div>
+    </div>
+    `
+    document.getElementById('word_modal').showModal()
 }
 
 const displayLevelWord = (words) => {
@@ -38,6 +83,7 @@ const displayLevelWord = (words) => {
             <h2 class="font-bangla text-4xl font-medium">নেক্সট Lesson এ যান</h2>
         </div>
         `
+        manageSpinner(false)
         return;
     }
 
@@ -45,16 +91,17 @@ const displayLevelWord = (words) => {
         const card = document.createElement('div')
         card.className = "bg-white text-center p-14 rounded-lg"
         card.innerHTML = `
-        <h2 class="font-bold text-3xl mb-4">${word.word? word.word : 'শব্দ পাওয়া যায়নি'}</h2>
+        <h2 class="font-bold text-3xl mb-4">${word.word ? word.word : 'শব্দ পাওয়া যায়নি'}</h2>
             <p class="text-lg mb-3">Meaning /Pronounciation</p>
-            <h2 class="font-bangla font-semibold text-2xl">"${word.meaning? word.meaning: 'অর্থ পাওয়া যায়নি'} / ${word.pronunciation? word.pronunciation: 'pronunciation পাওয়া যায়নি'}"</h2>
+            <h2 class="font-bangla font-semibold text-2xl">"${word.meaning ? word.meaning : 'অর্থ পাওয়া যায়নি'} / ${word.pronunciation ? word.pronunciation : 'pronunciation পাওয়া যায়নি'}"</h2>
             <div class="flex justify-between mt-10">
-                <button class="btn p-4 bg-[#1a91ff1a] hover:bg-[#1a90ff86] w-14 h-14 rounded-lg"><i class="fa-solid fa-circle-info"></i></button>
+                <button onclick="loadWordDetail(${word.id})" class="btn p-4 bg-[#1a91ff1a] hover:bg-[#1a90ff86] w-14 h-14 rounded-lg"><i class="fa-solid fa-circle-info"></i></button>
                 <button class=" btn p-4 bg-[#1a91ff1a] hover:bg-[#1a90ff86] w-14 h-14 rounded-lg"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         `
         wordContainer.append(card)
     })
+    manageSpinner(false)
 }
 
 
